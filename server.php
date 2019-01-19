ff8610e0143bbb4329827cd61784dcbe0cb46005
TanLin <39691304+tanlin50140137@users.noreply.github.com>
2019-01-19 23:20:13 +0800

    ws
    ws

--- server.php
+++ server.php
@@ -12,7 +12,7 @@ class WebsocketTest {
             //echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";           
             foreach( $this->request_id as $v )
             {
-            	if( $this->Server->isEstablished($v) )
+            	if( $this->Server->exist($v) )
             	{
             		$server->push($v, $frame->data);
             	}
