b6bb939274004d648c537a22244d948b0f50d90b
TanLin <39691304+tanlin50140137@users.noreply.github.com>
2019-02-21 22:49:06 +0800

    nlsm
    nlsm

--- server.php
+++ server.php
@@ -9,17 +9,14 @@ class WebsocketTest {
     public function __construct() {
         $this->server = new Swoole\WebSocket\Server("0.0.0.0", 9501);
         $this->server->on('open', function (swoole_websocket_server $server, $request) {
-            //echo "server: handshake success with fd{$request->fd}\n";
-            //$this->request_id[] = $request->fd;
+            echo "server: handshake success with fd{$request->fd}\n";
+            $this->request_id[] = $request->fd;
         });
         $this->server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
-            //echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";           
+            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";           
             foreach( $this->request_id as $v )
             {
-            	if( $server->exist($v) )
-            	{
-            		$server->push($v, $frame->data);
-            	}
+            	$server->push($v, $frame->data);
             }
         });
         $this->server->on('close', function ($ser, $fd) {
